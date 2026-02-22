065ff1a8aa089017777f5e7621a91d3b2c05d138
Anonymous <anonymous03233@gmail.com>
2026-02-22 01:39:02 -0500

    update

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,6 @@ export const SITE: Site = {
   TITLE: "Yunan Zhang | CS PhD Student",
   DESCRIPTION: "Personal academic website of a Computer Science PhD student.",
   EMAIL: "zhan4404@purdue.edu",
-  NUM_POSTS_ON_HOMEPAGE: 2,
   NUM_PUBLICATIONS_ON_HOMEPAGE: 5,
   SITEURL: "https://your-domain.com", // Update to your production domain for RSS and SEO.
 };
--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,6 @@ export type Site = {
   TITLE: string;
   DESCRIPTION: string;
   EMAIL: string;
-  NUM_POSTS_ON_HOMEPAGE: number;
   NUM_PUBLICATIONS_ON_HOMEPAGE: number;
   SITEURL: string,
 };
